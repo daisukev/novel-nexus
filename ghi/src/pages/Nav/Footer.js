703bfffb8502542b56667158558a7ab2df2a0b5a
Munti Cloud <muntazacloud@gmail.com>
2023-09-10 19:53:03 +0000

    Footer Logo

--- ghi/src/pages/Nav/Footer.js
+++ ghi/src/pages/Nav/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './styles/footer.css'
-import logo from '../../transparentlogo.png'
+import logo from '../../transparentlogo-light.png'
 
 function Footer(){
 
--- ghi/src/pages/profiles/settings/Settings.js
+++ ghi/src/pages/profiles/settings/Settings.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "../css/settings.css";
 import "../css/settings.module.css";
-import AvatarUpload from "./AvatarUpload";
+import AvatarUpload from "./AvatarUpload.jsx";
 import UpdateUserInfo from "./UpdateUserInfo";
 import Nav from "../../Nav/Nav";
 
