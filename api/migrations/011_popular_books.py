edf788f4524be8d01acad858400fcd590d4d2189
Joshua Daisuke Vasquez <daisuke@daisuke.dev>
2023-09-15 12:36:41 -0400

    fix table

--- api/migrations/011_popular_books.py
+++ api/migrations/011_popular_books.py
@@ -3,11 +3,16 @@ steps = [
         # "Up" SQL statement
         """
         CREATE TABLE popular_books (
-        popular_id SERIAL PRIMARY KEY,
-        book_id INTEGER REFERENCES books(id) ON DELETE CASCADE,
-        chapter_id INTEGER REFERENCES chapters(id) ON DELETE CASCADE,
-        visits INTEGER DEFAULT 0
-         );
+             id SERIAL PRIMARY KEY,
+             title VARCHAR(255) NOT NULL,
+             cover VARCHAR(255),
+             is_published BOOLEAN DEFAULT (FALSE),
+             created_at TIMESTAMP DEFAULT (NOW()),
+             updated_at TIMESTAMP,
+             visits INTEGER,
+             book_id INTEGER REFERENCES books(id) ON DELETE CASCADE,
+             author_id INTEGER REFERENCES authors(id) ON DELETE CASCADE
+        );
         """,
         # "Down" SQL statement
         """
